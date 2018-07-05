2e416b9eed934f7d689bd49bc3ed24aad81f6e28
Ewoma <tonyewoma@gmail.com>
2018-07-05 17:58:21 +0100

    my first comming - admin views created

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import {Header} from './components/common';
+import {Button, Header, Spinner} from "./components/common";
 import LoginForm from './components/LoginForm';
 
 
 
 class App extends Component {
+
+    state = { loggedIn: null };
 
     componentWillMount(){
         firebase.initializeApp(
@@ -19,13 +21,42 @@ class App extends Component {
                 messagingSenderId: '463261166347'
             }
         );
+
+        firebase.auth().onAuthStateChanged((user) =>{
+            if (user){
+                this.setState({ loggedIn: true });
+            } else{
+                this.setState({loggedIn: false});
+            }
+        });
+    }
+
+    renderContent(){
+        switch (this.state.loggedIn){
+            case true:
+                return (
+                    <View style={{ flexDirection: 'row', paddingTop: 10 }}>
+                    <Button onPress={() => firebase.auth().signOut()}>
+                        Log Out
+                    </Button>
+                    </View>
+                );
+            case false:
+                return <LoginForm/>;
+            default:
+                return (
+                    <View style={{ flexDirection: 'row', paddingTop: 10 }}>
+                        <Spinner size="large" />
+                    </View>
+                );
+        }
     }
 
     render() {
         return(
             <View>
                 <Header headerText = "Authentication"/>
-                <LoginForm/>
+                {this.renderContent()}
             </View>
         );
     }
